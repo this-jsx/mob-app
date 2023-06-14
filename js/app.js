56583e2ad3a416fd7422c093e1f590474faac82d
Artem Khannanov <Artem.Khannanov@coral-club>
2023-06-14 12:58:04 +0300

    375px

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ const headerItem1 = document.querySelector('.header__link--1')
 
 // const navigatorUserAgent = navigator.userAgent
 const visibleShopLink = document.querySelector('.footer__left-link_showed img')
+const visibleShopAndroid = document.querySelector('.footer__left-link--android img')
 // console.log(navigatorUserAgent);
 // console.log(visibleShopLink);
 
@@ -45,6 +46,11 @@ if (/iPhone/i.test(navigator.userAgent)) {
   visibleShopLink.src = 'img/download_playmarket.png'
 } else {
   visibleShopLink.src = 'img/download_rustore.png'
+}
+// console.log(window.screen.width);
+if (window.screen.width <= 430 || /Android/i.test(navigator.userAgent)) {
+  visibleShopAndroid.classList.remove('footer__left-link--hidden')
+  // visibleShopAndroid.src
 }
 
 // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -57,3 +63,40 @@ if (/iPhone/i.test(navigator.userAgent)) {
 //   console.log('вход с компа');
 //   headerItem1.style.color = 'blue'
 // }
+
+function copyClipboard(text) {
+  return navigator.clipboard.writeText(text)
+}
+
+const saleTooltip = document.querySelector('.sale__tooltip')
+const saleLink = document.querySelector('.sale__top-link')
+const saleImg = document.querySelector('.sale__top-link img')
+// console.log(saleTooltip);
+saleLink.addEventListener('click', function() {
+  // saleTooltip.clipboard.writeText('Скопировано')
+  copyClipboard('ссылка копи')
+  saleTooltip.textContent = 'Скопировано'
+})
+
+saleTooltip.addEventListener('click', function() {
+  // saleTooltip.clipboard.writeText('Скопировано')
+  copyClipboard('тултип копи')
+  saleTooltip.textContent = 'Скопировано'
+})
+
+// saleTooltip.addEventListener('mouseleave', function() {
+//   // saleTooltip.textContent = 'Копировать'
+// })
+
+const toolTipInitText = [saleLink, saleImg, saleTooltip]
+toolTipInitText.forEach(item => item.addEventListener('mouseenter', function() {
+  saleLink.style.background = '#877CCF'
+}))
+
+toolTipInitText.forEach(item => item.addEventListener('mouseleave', function() {
+  saleLink.style.background = 'transparent'
+}))
+
+saleLink.addEventListener('mouseleave', function() {
+  saleTooltip.textContent = 'Копировать'
+})
